f4934531b3f305bfe76adb5c7b7e0cb29c051435
Fukkong kyu <rivirtu@gmail.com>
2018-02-13 17:14:21 +0900

    Add Link to each HeadNav component and give some style. #15

--- src/components/HeadNav.js
+++ src/components/HeadNav.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import AppBar from 'material-ui/AppBar'
-import FlatButton from 'material-ui/FlatButton'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { FlatButton, AppBar } from 'material-ui'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 const localStyle = {
   navigationBar: {
     backgroundColor: '#FFFFFF',
+    height: '4rem'
   },
   navigationFont: {
     fontSize: '20',
@@ -14,28 +14,64 @@ const localStyle = {
   },
   loginButton: {
     //float: 'right',
-    align: 'center'
+    align: 'center',
+    height: '100%'
+  },
+  title: {
+    display: 'none'
+  },
+  logoStyle: {
+    fontWeight: 'bold',
+    fontSize: '4rem',
+    height: '100%'
   }
+
 }
 
 class HeadNav extends React.Component {
-  loginClick () {
-
-  }
-
   render () {
     return (
-      <div style={localStyle.navigationBar}>
-        <div style={localStyle.navigationFont}>
-          Unimo
-          <Link to="/login">
+      <AppBar
+        style={localStyle.navigationBar}
+        titleStyle={localStyle.title}
+        showMenuIconButton={false}
+      >
+        <div style={{flex: 1, textAlign: 'left', height: '100%'}}>
+          <Link to='/clubs'>
             <FlatButton
-              label="Login"
+              label="소모임 리스트"
+              style={localStyle.loginButton}
+            />
+          </Link>
+          <Link to='/new-club'>
+            <FlatButton
+              label="소모임 생성"
               style={localStyle.loginButton}
             />
           </Link>
         </div>
-      </div>
+        <div style={{flex: 1, textAlign: 'center', height: '100%'}}>
+          <FlatButton
+            label="소모임.공간"
+            labelStyle={{fontWeight: 'bold', fontSize: '3rem'}}
+            style={localStyle.logoStyle}
+          />
+        </div>
+        <div style={{flex: 1, textAlign: 'right', height: '100%'}}>
+          <Link to='/my-club'>
+            <FlatButton
+              label="나의 소모임"
+              style={localStyle.loginButton}
+            />
+          </Link>
+          <Link to='/login'>
+            <FlatButton
+              label="로그인"
+              style={localStyle.loginButton}
+            />
+          </Link>
+        </div>
+      </AppBar>
     )
   }
 }
