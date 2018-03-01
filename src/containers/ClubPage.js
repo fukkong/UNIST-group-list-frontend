022e9f7272c906571f4d7bde81c4ff7cd75dc18d
James <topfeeling03@google.com>
2018-03-01 01:09:41 -0800

    Show clublist detail data

--- src/containers/ClubPage.js
+++ src/containers/ClubPage.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react'
-import logo from '../logo.svg'
+import { api } from '../api'
 import HeadNav from '../components/HeadNav'
 import HomeHeader from '../components/HomeHeader'
-import ClubList from '../components/ClubList'
 import './Home.css'
 
+function getClubData (context) {
+  api.get('/club/' + context.state.clubId)
+    .then((response) => response.data)
+    .then((data) => context.setState({clubData : data}))
+}
+
 class ClubPage extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      clubId: props.match.params.id,
+      clubData: []
+    }
+    getClubData(this)
+  }
+
   render () {
     return (
-      <div>
-        To get started, edit <code>src/App.js</code> and save to reload.
+      <div className="App">
+        <HeadNav/>
+        <HomeHeader/>
+        <h1>clubname: {this.state.clubData['name']}</h1>
+        <h3><font color="red">introduce_one_line: {this.state.clubData['introduce_one_line']}</font></h3>
+        <h3>tel: {this.state.clubData['introduce_all']}</h3>
+        <h3>etc.: {this.state.clubData['created'], this.state.clubData['manager']}</h3>
       </div>
     )
   }
